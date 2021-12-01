bcb1c21cfcbfd93722ea9dab1a404be5f033b647
Doris Morales <dmorales55@gmail.com>
2021-11-30 21:38:47 -0500

    Migraciones, Factorias y Seeders

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,38 +1,34 @@
 <?php
 
+use App\Models\Role;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
-            $table->string('profile_photo_path', 2048)->nullable();
-            $table->timestamps();
-        });
-    }
+  public function up()
+  {
+    Schema::create('users', function (Blueprint $table) {
+      $table->id();
+      $table->foreignIdFor(Role::class)->constrained();
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
-    }
+      $table->string('name');
+      $table->string('email')->unique();
+      $table->enum('gender', ['male', 'female']);
+      $table->string('phone', 20);
+      $table->timestamp('email_verified_at')->nullable();
+      $table->string('password');
+      $table->foreignId('current_team_id')->nullable();
+      $table->string('profile_photo_path', 2048)->nullable();
+      $table->rememberToken();
+      $table->timestamps();
+    });
+  }
+
+  
+  public function down()
+  {
+    Schema::dropIfExists('users');
+  }
 }
